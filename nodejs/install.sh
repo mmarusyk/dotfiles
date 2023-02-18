a8c32be2edf2a1e84f110dfc0b6880a68db3abaf
Misha Marusyk <misha.marusyk@gmail.com>
2023-02-18 18:28:10 +0200

    Upgrade NodeJS version to 18.14.1

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,6 +1,6 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
-NODEJS_VERSION='16.17.0'
+NODEJS_VERSION='18.14.1'
 
 if ! command -v node >/dev/null 2>&1; then
   printf "$GREEN\nInstalling Node.js $NODEJS_VERSION...$NC\n"
