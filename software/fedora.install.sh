cd58ce6ab552571546a9b7210958dc25114af1d2
Michael Marusyk <misha.marusyk@gmail.com>
2024-01-06 18:32:02 +0200

    update fedora software

--- software/fedora.install.sh
+++ software/fedora.install.sh
@@ -1,9 +1,8 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-printf "$GREEN\nUpgrading Ubuntu Software...$NC\n"
-sudo apt-get update -qq
-sudo apt-get upgrade -yq
+printf "$GREEN\nUpgrading Fedora Software...$NC\n"
+sudo dnf update -yq
 
 # Additional packages
 ## ruby
@@ -25,19 +24,11 @@ sudo dnf install -yq git-core \
   perl-FindBin \
   perl-lib \
   perl-File-Compare \
-  perl-IPC-Cmd
+  perl-IPC-Cmd \
+  postresql-devel \
+  git \
+  gcc-c++ \
+  curl
 
 ## tmux: xclip
 sudo dnf install -yq xclip
-
-# git
-if ! command -v git >/dev/null 2>&1; then
-  printf "$GREEN\nInstalling git...$NC\n"
-	sudo dnf install -yq git
-fi
-
-# curl
-if ! command -v curl >/dev/null 2>&1; then
-  printf "$GREEN\nInstalling curl...$NC\n"
-	sudo dnf install -yq curl
-fi
