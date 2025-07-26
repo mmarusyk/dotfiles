fe50dbe4ebc14885cf5705a64127a9e6ca058b9c
Mykhailo Marusyk <mmarusyk1@gmail.com>
2025-07-26 17:49:25 +0200

    Add dictionary shortcut linux

--- install/shortcuts.sh
+++ install/shortcuts.sh
@@ -3,9 +3,11 @@
 set -e
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
   #
   # Emoji Picker
   #
+
   SHORTCUT_NAME="Emoji Picker"
   COMMAND="gnome-characters"
   KEYBINDING="<Control>period"  # Ctrl + .
@@ -37,6 +39,38 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$NEW_LIST"
 
   # Set up the actual shortcut values
+  gsettings set "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$NEW_BINDING_PATH" name "$SHORTCUT_NAME"
+  gsettings set "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$NEW_BINDING_PATH" command "$COMMAND"
+  gsettings set "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$NEW_BINDING_PATH" binding "$KEYBINDING"
+
+  #
+  # Dictionary
+  #
+
+  if ! command -v goldendict &> /dev/null; then
+    echo "Installing GoldenDict..."
+    sudo apt update
+    sudo apt install -y goldendict
+  fi
+
+  SHORTCUT_NAME="Open GoldenDict"
+  COMMAND="goldendict"
+  KEYBINDING="<Control><Alt>w"
+
+  NEW_BINDING_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-goldendict/"
+  NEW_BINDING_FORMAT="'$NEW_BINDING_PATH'"
+
+  CURRENT_LIST=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
+
+  if [[ "$CURRENT_LIST" == "@as []" ]]; then
+    NEW_LIST="[$NEW_BINDING_FORMAT]"
+  elif [[ "$CURRENT_LIST" == *"$NEW_BINDING_PATH"* ]]; then
+    NEW_LIST="$CURRENT_LIST"
+  else
+    NEW_LIST="${CURRENT_LIST%]*}, $NEW_BINDING_FORMAT]"
+  fi
+
+  gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$NEW_LIST"
   gsettings set "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$NEW_BINDING_PATH" name "$SHORTCUT_NAME"
   gsettings set "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$NEW_BINDING_PATH" command "$COMMAND"
   gsettings set "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$NEW_BINDING_PATH" binding "$KEYBINDING"
