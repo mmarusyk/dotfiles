a315724277ea7dcb17c4411c88c161ff03e44959
Mykhailo Marusyk <mmarusyk1@gmail.com>
2025-10-17 23:46:26 +0200

    Save identification to ~/.dotfiles.env

--- install/1-identification.sh
+++ install/1-identification.sh
@@ -12,4 +12,12 @@ echo -e "\nYour identification:"
 echo "   Name : $DOTFILES_USER_NAME"
 echo "   Email: $DOTFILES_USER_EMAIL"
 
+echo -e "\nSaving identification to ~/.dotfiles.env..."
+cat > ~/.dotfiles.env << EOF
+export DOTFILES_USER_NAME="$DOTFILES_USER_NAME"
+export DOTFILES_USER_EMAIL="$DOTFILES_USER_EMAIL"
+EOF
+
+echo "Identification saved successfully!"
+
 sleep 3
